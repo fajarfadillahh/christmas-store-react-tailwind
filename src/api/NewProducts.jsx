66e6cda63a3176b67aa00f68162a655eb74ffe72
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-20 22:17:38 +0700

    modified Gift

---
+++ src/api/NewProducts.jsx
@@ -0,0 +1,28 @@
+const NewProducts = [
+  {
+    id: 1,
+    image: "http://localhost:5173/src/assets/images/new1.png",
+    price: "24.99",
+    text: "Reindeer",
+  },
+  {
+    id: 2,
+    image: "http://localhost:5173/src/assets/images/new2.png",
+    price: "27.99",
+    text: "Snow Globe",
+  },
+  {
+    id: 3,
+    image: "http://localhost:5173/src/assets/images/new3.png",
+    price: "22.99",
+    text: "Sledge",
+  },
+  {
+    id: 4,
+    image: "http://localhost:5173/src/assets/images/new4.png",
+    price: "18.99",
+    text: "Chirstmas Wreath",
+  },
+];
+
+export default NewProducts;
---
+++ src/api/Products.jsx
@@ -0,0 +1,34 @@
+const Products = [
+  {
+    id: 1,
+    image: "http://localhost:5173/src/assets/images/product1.png",
+    price: "14.99",
+    text: "Gingerbread",
+  },
+  {
+    id: 2,
+    image: "http://localhost:5173/src/assets/images/product2.png",
+    price: "29.99",
+    text: "Santa Claus Hat",
+  },
+  {
+    id: 3,
+    image: "http://localhost:5173/src/assets/images/product3.png",
+    price: "19.99",
+    text: "Christmas Tree",
+  },
+  {
+    id: 4,
+    image: "http://localhost:5173/src/assets/images/product4.png",
+    price: "21.99",
+    text: "Snowman",
+  },
+  {
+    id: 5,
+    image: "http://localhost:5173/src/assets/images/product5.png",
+    price: "12.99",
+    text: "Candy Stick",
+  },
+];
+
+export default Products;
