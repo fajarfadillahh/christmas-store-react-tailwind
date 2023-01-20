5b3b50ac359e02b45fb63f25c4481b2ee289b5cc
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-20 22:42:32 +0700

    modified api Products

--- src/api/Products.jsx
+++ src/api/Products.jsx
@@ -1,31 +1,36 @@
 const Products = [
   {
     id: 1,
-    image: "http://localhost:5173/src/assets/images/product1.png",
+    image:
+      "https://christmas-landing-page-tailwind-vite.vercel.app/assets/gift1.00270fa3.png",
     price: "14.99",
     text: "Gingerbread",
   },
   {
     id: 2,
-    image: "http://localhost:5173/src/assets/images/product2.png",
+    image:
+      "https://christmas-landing-page-tailwind-vite.vercel.app/assets/gift2.13c9c67d.png",
     price: "29.99",
     text: "Santa Claus Hat",
   },
   {
     id: 3,
-    image: "http://localhost:5173/src/assets/images/product3.png",
+    image:
+      "https://christmas-landing-page-tailwind-vite.vercel.app/assets/gift3.bfe1106b.png",
     price: "19.99",
     text: "Christmas Tree",
   },
   {
     id: 4,
-    image: "http://localhost:5173/src/assets/images/product4.png",
+    image:
+      "https://christmas-landing-page-tailwind-vite.vercel.app/assets/gift4.558d82e1.png",
     price: "21.99",
     text: "Snowman",
   },
   {
     id: 5,
-    image: "http://localhost:5173/src/assets/images/product5.png",
+    image:
+      "https://christmas-landing-page-tailwind-vite.vercel.app/assets/gift5.85b6a799.png",
     price: "12.99",
     text: "Candy Stick",
   },
