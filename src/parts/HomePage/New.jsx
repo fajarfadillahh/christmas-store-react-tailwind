303aaf5f2a77b1e23d22977779e30600f28804d1
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-21 14:15:50 +0700

    responsive New

--- src/parts/HomePage/New.jsx
+++ src/parts/HomePage/New.jsx
@@ -13,7 +13,7 @@ export default function New() {
           <h1 className="new__title section-title">New Products Gifts</h1>
         </div>
 
-        <div className="new__group grid grid-cols-2 gap-6">
+        <div className="new__group grid grid-cols-2 gap-6 md:grid-cols-3 lg:gap-x-12 lg:gap-y-8">
           {DataNewProducts.map((item) => (
             <NewCard
               key={item.id}
